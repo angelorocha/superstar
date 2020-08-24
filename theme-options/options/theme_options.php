2daa06bbed8b3e9cb100e2615677829602a2a1a6
Angelo Rocha <contato@angelorocha.com.br>
2020-08-23 22:57:01 -0300

    Update theme options initial config

--- theme-options/options/theme_options.php
+++ theme-options/options/theme_options.php
@@ -19,20 +19,20 @@ $opt_name = 'wpss_option';
 $theme = wp_get_theme();
 
 $args = array(
-    'display_name'     => $theme->get('Name'),
-    'display_version'  => $theme->get('Version'),
-    'menu_icon'        => 'dashicons-star-filled',
-    'menu_type'        => 'menu',
-    'menu_title'       => esc_html__('Theme Options', 'wpss'),
+    'display_name'       => $theme->get('Name'),
+    'display_version'    => $theme->get('Version'),
+    'menu_icon'          => 'dashicons-star-filled',
+    'menu_type'          => 'menu',
+    'menu_title'         => esc_html__('Theme Options', 'wpss'),
     //'page_parent'     => 'themes.php',
-    'page_priority'    => 9,
-    'page_permissions' => 'manage_options',
-    'page_slug'        => '_wpss_options_page',
-    'page_title'       => 'Theme Options Page',
+    'page_priority'      => 9,
+    'page_permissions'   => 'manage_options',
+    'page_slug'          => '_wpss_options_page',
+    'page_title'         => 'Theme Options Page',
     'show_import_export' => false,
-    'customizer'       => true,
-    'dev_mode'         => false,
-    'footer_text'      => '',
+    'customizer'         => true,
+    'dev_mode'           => false,
+    'footer_text'        => '',
 );
 
 $args['share_icons'][] = array(
@@ -55,24 +55,48 @@ $args['share_icons'][] = array(
     'title' => 'Follow us on YouTube',
     'icon'  => 'el el-youtube'
 );
+$args['share_icons'][] = array(
+    'url'   => 'https://github.com/angelorocha/superstar',
+    'title' => 'Github',
+    'icon'  => 'el el-github'
+);
 
 Redux::setArgs($opt_name, $args);
 
 Redux::setSection($opt_name, array(
-    'title'  => esc_html__('Basic Field', 'wpss'),
-    'id'     => 'basic',
-    'desc'   => esc_html__('Basic field with no subsections.', 'wpss'),
+    'title'  => esc_html__('Header', 'wpss'),
+    'id'     => 'header_options',
+    'desc'   => esc_html__('Theme header options.', 'wpss'),
     'icon'   => 'el el-home',
     'fields' => array(
         array(
-            'id'       => 'opt-text',
+            'id'       => 'wpss-header-logo',
             'type'     => 'media',
-            'title'    => esc_html__('Example Text', 'wpss'),
-            'desc'     => esc_html__('Example description.', 'wpss'),
-            'subtitle' => esc_html__('Example subtitle.', 'wpss'),
-            'hint'     => array(
-                'content' => 'This is a <b>hint</b> tool-tip for the text field.<br/><br/>Add any HTML based text you like here.',
-            )
+            'title'    => esc_html__('Site Logo', 'wpss'),
+            'desc'     => esc_html__('Define your site logo.', 'wpss'),
+            'subtitle' => esc_html__('Upload your logo file.', 'wpss'),
+            //'hint'     => array(
+            //    'content' => '',
+            //)
+        )
+    )
+));
+
+Redux::setSection($opt_name, array(
+    'title'  => esc_html__('Custom Scripts', 'wpss'),
+    'id'     => 'custom_scripts',
+    'desc'   => esc_html__('Insert your custom scripts here.', 'wpss'),
+    'icon'   => 'el el-jquery',
+    'fields' => array(
+        array(
+            'id'       => 'wpss-header-script',
+            'type'     => 'ace_editor',
+            'title'    => __('Header Javascript Code', 'wpss'),
+            'subtitle' => __('Paste your javascript code here.', 'wpss'),
+            'mode'     => 'javascript',
+            'theme'    => 'monokai',
+            'desc'     => esc_html__('Your script will be inserted immediately before the </head> tag', 'wpss'),
+            'default'  => "$('#your-script-here');"
         )
     )
 ));
